3daa8731717fd4132a9d73d787c9fe058cc8397f
Paulus Schoutsen <balloob@gmail.com>
2019-03-11 11:49:20 -0700

    Add managed boolean to cloudhooks

--- hass_nabucasa/cloudhooks.py
+++ hass_nabucasa/cloudhooks.py
@@ -14,6 +14,9 @@ class Cloudhooks:
 
     async def async_publish_cloudhooks(self):
         """Inform the Relayer of the cloudhooks that we support."""
+        if not self.cloud.is_connected:
+            return
+
         cloudhooks = self.cloud.client.cloudhooks
         await self.cloud.iot.async_send_message(
             "webhook-register",
@@ -21,7 +24,7 @@ class Cloudhooks:
             expect_answer=False,
         )
 
-    async def async_create(self, webhook_id):
+    async def async_create(self, webhook_id, managed):
         """Create a cloud webhook."""
         cloudhooks = self.cloud.client.cloudhooks
 
@@ -45,10 +48,12 @@ class Cloudhooks:
             "webhook_id": webhook_id,
             "cloudhook_id": cloudhook_id,
             "cloudhook_url": cloudhook_url,
+            "managed": managed,
         }
         await self.cloud.client.async_cloudhooks_update(cloudhooks)
 
         await self.async_publish_cloudhooks()
+
         return hook
 
     async def async_delete(self, webhook_id):
