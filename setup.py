38cd42441502c80fb7c82955b62974c9c1ef5355
Pascal Vizeli <pascal.vizeli@syshack.ch>
2019-03-21 11:56:40 +0100

    Update snitun 0.16 (#38)

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     packages=["hass_nabucasa"],
     install_requires=[
         "warrant==0.6.1",
-        "snitun==0.15",
+        "snitun==0.16",
         "acme==0.32.0",
         "cryptography>=2.5",
         "attrs>=18.2.0",
