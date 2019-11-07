a42f3e927c4720eae9529c22fdbfb5d7192c21ba
Paulus Schoutsen <balloob@gmail.com>
2019-11-07 13:48:17 -0800

    Handle TT errors

--- hass_nabucasa/thingtalk.py
+++ hass_nabucasa/thingtalk.py
@@ -6,10 +6,25 @@ if TYPE_CHECKING:
     from . import Cloud
 
 
+class ThingTalkConversionError(Exception):
+    """Conversion error occurred."""
+
+
 async def async_convert(cloud: "Cloud", query: str):
     """Convert sentence."""
     resp = await cloud.client.websession.post(
         f"{cloud.thingtalk_url}/convert", json={"query": query}
     )
-    resp.raise_for_status()
-    return await resp.json()
+    if resp.status == 200:
+        return await resp.json()
+
+    try:
+        body = await resp.json()
+    except ValueError:
+        # Invalid JSON in body
+        resp.raise_for_status()
+
+    if not isinstance(body, dict) or "error" not in body:
+        resp.raise_for_status()
+
+    raise ThingTalkConversionError(body["error"])
---
+++ tests/test_thingtalk.py
@@ -0,0 +1,39 @@
+"""Tests for ThingTalk."""
+import pytest
+
+import aiohttp
+
+from hass_nabucasa import thingtalk
+
+API_URL = "https://example.com"
+CONVERT_URL = f"{API_URL}/convert"
+
+
+@pytest.fixture(autouse=True)
+def set_api_url(cloud_mock):
+    """Set TT API url."""
+    cloud_mock.thingtalk_url = API_URL
+
+
+async def test_async_convert_ok(aioclient_mock, cloud_mock):
+    """Test async convert."""
+    aioclient_mock.post(CONVERT_URL, json={"hello": "yo"})
+    assert await thingtalk.async_convert(cloud_mock, "Hello") == {"hello": "yo"}
+
+
+async def test_async_convert_fail(aioclient_mock, cloud_mock):
+    """Test async convert."""
+    aioclient_mock.post(CONVERT_URL, status=400, json={"error": "Convert Error!"})
+    with pytest.raises(thingtalk.ThingTalkConversionError) as excinfo:
+        await thingtalk.async_convert(cloud_mock, "Hello")
+
+    assert str(excinfo.value) == "Convert Error!"
+
+
+async def test_async_convert_invalid(aioclient_mock, cloud_mock):
+    """Test async convert."""
+    aioclient_mock.post(CONVERT_URL, status=500, text="")
+    with pytest.raises(aiohttp.ClientResponseError) as excinfo:
+        await thingtalk.async_convert(cloud_mock, "Hello")
+
+    assert excinfo.value.status == 500
