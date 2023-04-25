cf64c6740590e0ae3cfb5ac10c307257007018cb
ludeeus <joasoe@gmail.com>
2023-04-25 10:25:59 +0000

    Use servicehandlers server for voice

--- tests/test_cloud_api.py
+++ tests/test_cloud_api.py
@@ -102,9 +102,9 @@ async def test_get_access_token(auth_cloud_mock, aioclient_mock):
 
 async def test_voice_connection_details(auth_cloud_mock, aioclient_mock):
     """Test creating a cloudhook."""
-    aioclient_mock.get("https://example.com/connection_details")
+    aioclient_mock.get("https://example.com/voice/connection_details")
     auth_cloud_mock.id_token = "mock-id-token"
-    auth_cloud_mock.voice_server = "example.com"
+    auth_cloud_mock.servicehandlers_server = "example.com"
 
     await cloud_api.async_voice_connection_details(auth_cloud_mock)
     assert len(aioclient_mock.mock_calls) == 1
