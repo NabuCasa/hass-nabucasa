125b3af7587ed2333588e18806102a5419f6cff5
Erik <erik@montnemery.com>
2023-05-04 12:09:49 +0200

    Black

--- hass_nabucasa/account_link.py
+++ hass_nabucasa/account_link.py
@@ -124,7 +124,9 @@ async def async_fetch_access_token(
     return tokens
 
 
-async def async_fetch_available_services(cloud: Cloud[_ClientT]) -> list[dict[str, Any]]:
+async def async_fetch_available_services(
+    cloud: Cloud[_ClientT],
+) -> list[dict[str, Any]]:
     """Fetch available services."""
 
     resp = await cloud.client.websession.get(
