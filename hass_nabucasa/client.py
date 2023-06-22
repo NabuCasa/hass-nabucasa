bb2b2984cde2b3673a4b1f09274d87d035604494
Joakim Sørensen <ludeeus@ludeeus.dev>
2023-06-22 14:45:24 +0200

    Update hass_nabucasa/client.py

--- hass_nabucasa/client.py
+++ hass_nabucasa/client.py
@@ -74,7 +74,7 @@ class CloudClient(ABC):
 
     @abstractmethod
     async def async_cloud_connection_info(
-        self, payload: dict[Any, Any]
+        self, payload: dict[str, Any]
     ) -> dict[str, Any]:
         """Process cloud connection info message to client."""
 
