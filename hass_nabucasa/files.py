c3299e27b23c18cca8ac9ef598b27aa29c1ec81c
ludeeus <joasoe@gmail.com>
2025-01-31 14:23:24 +0000

    format

--- hass_nabucasa/files.py
+++ hass_nabucasa/files.py
@@ -89,15 +89,13 @@ class Files(ApiBase):
                 details["url"],
                 data=await open_stream(),
                 headers=details["headers"] | {"content-length": str(size)},
-                timeout=ClientTimeout(
-                    connect=10.0, total=_FILE_TRANSFER_TIMEOUT),
+                timeout=ClientTimeout(connect=10.0, total=_FILE_TRANSFER_TIMEOUT),
             )
             self._do_log_response(response)
 
             response.raise_for_status()
         except TimeoutError as err:
-            raise FilesError(
-                "Timeout reached while trying to upload file") from err
+            raise FilesError("Timeout reached while trying to upload file") from err
         except ClientError as err:
             raise FilesError("Failed to upload file") from err
         except Exception as err:
@@ -122,8 +120,7 @@ class Files(ApiBase):
         try:
             response = await self._cloud.websession.get(
                 details["url"],
-                timeout=ClientTimeout(
-                    connect=10.0, total=_FILE_TRANSFER_TIMEOUT),
+                timeout=ClientTimeout(connect=10.0, total=_FILE_TRANSFER_TIMEOUT),
             )
 
             self._do_log_response(response)
@@ -132,12 +129,10 @@ class Files(ApiBase):
         except FilesError:
             raise
         except TimeoutError as err:
-            raise FilesError(
-                "Timeout reached while trying to download file") from err
+            raise FilesError("Timeout reached while trying to download file") from err
         except ClientError as err:
             raise FilesError("Failed to download file") from err
         except Exception as err:
-            raise FilesError(
-                "Unexpected error while downloading file") from err
+            raise FilesError("Unexpected error while downloading file") from err
 
         return response.content
