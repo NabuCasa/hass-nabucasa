b42b1900116eb528a7bcf1a4a2c722d19a571198
Joakim Sørensen <joasoe@proton.me>
2026-09-11 15:04:22 +0200

    Extend AWS_EXCEPTIONS (#1196)

--- hass_nabucasa/auth/cognito.py
+++ hass_nabucasa/auth/cognito.py
@@ -10,7 +10,7 @@ from typing import TYPE_CHECKING, Any
 
 import boto3
 import botocore
-from botocore.exceptions import BotoCoreError, ClientError, EndpointConnectionError
+from botocore.exceptions import BotoCoreError, ClientError
 import pycognito
 from pycognito.exceptions import ForceChangePasswordException, MFAChallengeException
 
@@ -107,7 +107,11 @@ AWS_EXCEPTIONS: dict[str, type[CloudError]] = {
     "UsernameExistsException": UserExists,
     "NotAuthorizedException": Unauthenticated,
     "PasswordResetRequiredException": PasswordChangeRequired,
+    "ConnectionClosedError": CloudConnectionError,
+    "ConnectTimeoutError": CloudConnectionError,
     "EndpointConnectionError": CloudConnectionError,
+    "ProxyConnectionError": CloudConnectionError,
+    "ReadTimeoutError": AuthTimeoutError,
 }
 
 
@@ -184,10 +188,8 @@ class CognitoAuth:
                     ),
                 )
 
-        except (ClientError, EndpointConnectionError) as err:
+        except (ClientError, BotoCoreError) as err:
             raise _map_aws_exception(err) from err
-        except BotoCoreError as err:
-            raise UnknownError from err
 
     async def async_resend_email_confirm(self, email: str) -> None:
         """Resend email confirmation."""
@@ -203,10 +205,8 @@ class CognitoAuth:
                         ClientId=cognito.client_id,
                     ),
                 )
-        except (ClientError, EndpointConnectionError) as err:
+        except (ClientError, BotoCoreError) as err:
             raise _map_aws_exception(err) from err
-        except BotoCoreError as err:
-            raise UnknownError from err
 
     async def async_forgot_password(self, email: str) -> None:
         """Initialize forgotten password flow."""
@@ -217,10 +217,8 @@ class CognitoAuth:
                 )
                 await self.cloud.run_executor(cognito.initiate_forgot_password)
 
-        except (ClientError, EndpointConnectionError) as err:
+        except (ClientError, BotoCoreError) as err:
             raise _map_aws_exception(err) from err
-        except BotoCoreError as err:
-            raise UnknownError from err
 
     async def async_login(
         self,
@@ -267,11 +265,8 @@ class CognitoAuth:
         except TimeoutError as err:
             raise AuthTimeoutError("Timeout while logging in") from err
 
-        except (ClientError, EndpointConnectionError) as err:
+        except (ClientError, BotoCoreError) as err:
             raise _map_aws_exception(err) from err
-
-        except BotoCoreError as err:
-            raise UnknownError from err
 
     async def async_login_verify_totp(
         self,
@@ -317,11 +312,8 @@ class CognitoAuth:
         except TimeoutError as err:
             raise AuthTimeoutError("Timeout while verifying TOTP code") from err
 
-        except (ClientError, EndpointConnectionError) as err:
+        except (ClientError, BotoCoreError) as err:
             raise _map_aws_exception(err) from err
-
-        except BotoCoreError as err:
-            raise UnknownError from err
 
     async def async_check_token(self) -> None:
         """Check that the token is valid and renew if necessary."""
@@ -365,11 +357,8 @@ class CognitoAuth:
         except TimeoutError as err:
             raise AuthTimeoutError("Timeout while renewing access token") from err
 
-        except (ClientError, EndpointConnectionError) as err:
+        except (ClientError, BotoCoreError) as err:
             raise _map_aws_exception(err) from err
-
-        except BotoCoreError as err:
-            raise UnknownError from err
 
     async def _async_authenticated_cognito(self) -> pycognito.Cognito:
         """Return an authenticated cognito instance."""
